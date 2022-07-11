c6a758720cb7ecb0af346b24cb4871def2c6ccfc
sanskar choudhry <sanskar22101@gmail.com>
2022-07-11 11:53:19 +0530

    added homepage, menu item and directory components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <HomePage />
     </div>
   );
 }
